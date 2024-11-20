4c5425c42d8774cb4d9feaaa4a29d6cbfcc141fe
minilustmirage <bbetsalil13@gmail.com>
2024-11-20 11:57:31 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "𝗚𝗛𝗢𝗦𝗧-𝗠𝗗",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94741140620",
+  OWNER_NAME: process.env.OWNER_NAME || "𝗚𝗛𝗢𝗦𝗧-𝐃𝐄𝐍𝐉𝐈",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2250555581921",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
